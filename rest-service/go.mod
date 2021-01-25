e980786ba7d06f0cd1cccbaad739c7f705bd5d6b
Kamil Mikolajczyk <k4mil.mikolajczyk+gh@gmail.com>
2021-01-25 14:06:08 +0100

    converted server to echo

--- rest-service/go.mod
+++ rest-service/go.mod
@@ -2,4 +2,4 @@ module github.com/reg0/go-workshop
 
 go 1.15
 
-require github.com/labstack/echo/v4 v4.1.17 // indirect
+require github.com/labstack/echo/v4 v4.1.17
--- rest-service/main.go
+++ rest-service/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"github.com/labstack/echo/v4"
 	"github.com/reg0/go-workshop/hello"
 	"net/http"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", hello.SayHello)
+	e := echo.New()
+	e.GET("/hello", echo.WrapHandler(http.HandlerFunc(hello.SayHello)))
 
-	err := http.ListenAndServe(":8080", mux)
+	//mux := http.NewServeMux()
+	//mux.HandleFunc("/hello", hello.SayHello)
+	//
+	//err := http.ListenAndServe(":8080", mux)
+
+	err := e.Start(":8080")
 
 	if err != nil {
 		panic(err)
